5a5436d719c349886a9eab40abc348bcb2fe00cb
tetreaultmax <maxime.tetreault@polymtl.ca>
2023-07-06 15:06:30 -0400

    toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppComponent } from './app.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule,  MatSelectModule, MatButtonModule, BrowserAnimationsModule
+    BrowserModule, FormsModule,  MatSelectModule, MatButtonModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
